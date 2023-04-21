1dcb21d271c42009df65b7acea830054aef58a7e
kwlee365 <kwlee365@snu.ac.kr>
2023-04-21 09:19:33 +0900

    add Rviz visualization

--- src/imu_tf_broadcaster.cpp
+++ src/imu_tf_broadcaster.cpp
@@ -1,84 +1,40 @@
-#include <ros/ros.h>
-#include <geometry_msgs>
 #include <sensor_msgs/Imu.h>
+#include <ros/ros.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
 #include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+
+void CallbackIMUdata(const sensor_msgs::Imu imu_msg)
+{
+    static tf2_ros::TransformBroadcaster br;
+    geometry_msgs::TransformStamped tr;
+
+    tr.header.stamp = ros::Time::now();
+    tr.header.frame_id = "map";
+    tr.child_frame_id = "imu_link";
+    tr.transform.translation.x = 0.0;
+    tr.transform.translation.y = 0.0;
+    tr.transform.translation.z = 3.0;
+
+    // tf2::Quaternion q;
+
+    tr.transform.rotation.x = imu_msg.orientation.x;
+    tr.transform.rotation.y = imu_msg.orientation.y;
+    tr.transform.rotation.z = imu_msg.orientation.z;
+    tr.transform.rotation.w = imu_msg.orientation.w;
+
+    br.sendTransform(tr);
+}
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "imu_tf_broadcaster");
-    ros::NodeHandle n;
-    ros::Rate r(1);
+    ros::NodeHandle nh_;
 
+    ros::Subscriber imu_sub = nh_.subscribe("/imu/data", 10, &CallbackIMUdata);
 
-    while (ros::ok())
-    {
-        visualization_msgs::Marker marker;
-        // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-        marker.header.frame_id = "/my_frame";
-        marker.header.stamp = ros::Time::now();
-
-        // Set the namespace and id for this marker.  This serves to create a unique ID
-        // Any marker sent with the same namespace and id will overwrite the old one
-        marker.ns = "basic_shapes";
-        marker.id = 0;
-
-        // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
-        marker.type = shape;
-
-        // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-        marker.action = visualization_msgs::Marker::ADD;
-
-        // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-        marker.pose.position.x = 0;
-        marker.pose.position.y = 0;
-        marker.pose.position.z = 0;
-        marker.pose.orientation.x = 0.0;
-        marker.pose.orientation.y = 0.0;
-        marker.pose.orientation.z = 0.0;
-        marker.pose.orientation.w = 1.0;
-
-        // Set the scale of the marker -- 1x1x1 here means 1m on a side
-        marker.scale.x = 1.0;
-        marker.scale.y = 1.0;
-        marker.scale.z = 1.0;
-
-        // Set the color -- be sure to set alpha to something non-zero!
-        marker.color.r = 0.0f;
-        marker.color.g = 1.0f;
-        marker.color.b = 0.0f;
-        marker.color.a = 1.0;
-
-        marker.lifetime = ros::Duration();
-
-        // Publish the marker
-        while (marker_pub.getNumSubscribers() < 1)
-        {
-            if (!ros::ok())
-            {
-                return 0;
-            }
-            ROS_WARN_ONCE("Please create a subscriber to the marker");
-            sleep(1);
-        }
-        marker_pub.publish(marker);
-
-        // Cycle between different shapes
-        switch (shape)
-        {
-        case visualization_msgs::Marker::CUBE:
-            shape = visualization_msgs::Marker::SPHERE;
-            break;
-        case visualization_msgs::Marker::SPHERE:
-            shape = visualization_msgs::Marker::ARROW;
-            break;
-        case visualization_msgs::Marker::ARROW:
-            shape = visualization_msgs::Marker::CYLINDER;
-            break;
-        case visualization_msgs::Marker::CYLINDER:
-            shape = visualization_msgs::Marker::CUBE;
-            break;
-        }
-
-        r.sleep();
-    }
-}
+    ros::spin();
+    return 0;
+};
